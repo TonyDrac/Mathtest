ce5a9165d71cc1f5624d40e4a20d3cb46e26af21
Tony Dracéus <44443117+TonyDrac@users.noreply.github.com>
2019-04-03 09:53:09 +0200

    Add files via upload

---
+++ Matte.java
@@ -0,0 +1,25 @@
+package com.hmkcode.junit;
+
+public class Matte {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public double sum(double x, double y) {
+        return x + y;
+    }
+
+    public double multiply(double x, double y) {
+        return x * y;
+    }
+
+    public double divide(double x, double y) {
+        return x / y;
+    }
+
+    public double subtract(double x, double y) {
+        return x - y;
+    }
+}
+
+
